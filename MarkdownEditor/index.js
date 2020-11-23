28c805f42c3b53785b8441d4272888185243d738
matt011 <38515217+matt011@users.noreply.github.com>
2020-11-23 19:39:02 +0800

    make editable and render markdown

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import css from './style.css';
+import ReactMarkdown from 'react-markdown';
+import path from 'path';
 
 function MarkdownEditor({ file, write }) {
   console.log(file, write);
+  const [value, setValue] = useState('# sdahdsuiahduishda');
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    async () => setValue(await file.text());
+  }, [file]);
+
+  const onSave = () => {
+    console.log('Saving File');
+    const newFile = new File([value], file.name, {
+      type: 'text/markdown',
+      lastModified: new Date()
+    });
+
+    write(newFile, value);
+  };
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
+      <div className={css.title}>
+        <div>{path.basename(file.name)}</div>
+        {!isEditing && <div onClick={() => setIsEditing(!isEditing)}>EDIT</div>}
+      </div>
+      {isEditing ? (
+        <textarea
+          className={css.content}
+          autoFocus="true"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          onBlur={e => {
+            e.preventDefault();
+            onSave();
+            setIsEditing(!isEditing);
+          }}
+        />
+      ) : (
+        <ReactMarkdown className={css.content}>{value}</ReactMarkdown>
+      )}
     </div>
   );
 }
